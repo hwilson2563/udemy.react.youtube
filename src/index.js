1f95128b75f38d262c85889112455db2734b570b
Hilary Claire <hilary.wilson2563@gmail.com>
2018-02-14 15:29:01 -0500

    api request

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 const API_KEY = 'AIzaSyDsc2xJdCmsVA85qxXHqw0l2pn9WLfvHFw';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+	console.log(data);
+})
+
 const App = () => {
 	return (
 		<div>
