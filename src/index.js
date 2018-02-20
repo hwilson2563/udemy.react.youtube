c664a77718b61b60448d820bb7797d71260d8b03
Hilary Claire <hilary.wilson2563@gmail.com>
2018-02-19 19:27:32 -0500

    being able to select from list and able to play first video as default

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import YTSearch from 'youtube-api-search';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDsc2xJdCmsVA85qxXHqw0l2pn9WLfvHFw';
 
 
@@ -12,17 +13,22 @@ class App extends Component {
 	constructor(props){
 		super(props);
 		this.state ={
-			videos:[]
+			videos:[],
+			selectedVideo: null
 		};
 		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-		this.setState({videos});
+		this.setState({
+			videos:videos,
+			selectedVideo : videos[0]
+		});
 })
 	}
 	render(){
 	return (
 		<div>
 				<SearchBar />
-				<VideoList videos={this.state.videos}/>
+				<VideoDetail video={this.state.selectedVideo}/>
+				<VideoList videos={this.state.videos} onVideoSelect={selectedVideo => this.setState({selectedVideo}) }/>
 		</div>
 		);
 	}
